e4ea44673e7fbbb4d039d28f71f1ca459f65d896
Joanna <5525asd9896@gmail.com>
2024-06-05 16:28:35 +0800

    下課 save：路由跳轉ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelCommentComponent } from './hotel-comment/hotel-comment.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/hotel_comment', pathMatch: 'full' },
+  { path: 'hotel_comment', component: HotelCommentComponent }, //其他人接過來要這樣設定
+  { path: 'comment_list', loadChildren: () => import('./comment/comment.module').then(m => m.CommentModule) } //蓉自己另外設定
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
